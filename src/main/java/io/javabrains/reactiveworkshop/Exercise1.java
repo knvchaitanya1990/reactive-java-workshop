dd83c2bb5b3ad0af22c5f3f484d09b0cd5ab088d
chaitanya <chaitanya.kattunga@gmail.com>
2023-08-15 11:46:19 +0200

    Commited code for excercise  procblems

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,30 +1,58 @@
 package io.javabrains.reactiveworkshop;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
 
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
-        // Print all numbers in the intNumbersStream stream
+        System.out.println("<--- Print all numbers in the intNumbersStream stream -->");
         // TODO: Write code here
 
-        // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream().forEach(System.out::println);
 
-        // Print the second and third numbers in intNumbersStream that's greater than 5
+        System.out.println("<--- Print numbers from intNumbersStream that are less than 5 -->");
         // TODO: Write code here
+        StreamSources.intNumbersStream().filter(n -> n < 5).
+                forEach(System.out::println);
+        System.out.println("<--- Print the second and third numbers in intNumbersStream that's greater than 5 -->");
+        // TODO: Write code here
+        StreamSources.intNumbersStream().filter(n -> n > 5).skip(1).limit(2).
+                forEach(System.out::println);
 
-        //  Print the first number in intNumbersStream that's greater than 5.
+        System.out.println("Print the first number in intNumbersStream that's greater than 5.");
         //  If nothing is found, print -1
         // TODO: Write code here
 
-        // Print first names of all users in userStream
+        Integer input = StreamSources.intNumbersStream().filter(n -> n > 5).
+                findFirst().
+                orElse(-1);
+        System.out.println(input);
+
+        System.out.println("<-- Print first names of all users in userStream -->");
+        // TODO: Write code here
+        StreamSources.userStream().map(User::getFirstName).forEach(System.out::println);
+
+        System.out.println("<-- Print first names in userStream for users that have IDs from number stream-->");
         // TODO: Write code here
 
-        // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        System.out.println("#### First  Option #### ");
+        Stream<Integer> numberStream = StreamSources.intNumbersStream();
+        Set<Integer> numberSet = numberStream.collect(Collectors.toSet());
 
+        StreamSources.userStream()
+                .filter(user -> numberSet.contains(user.getId()))
+                .map(User::getFirstName)
+                .forEach(System.out::println);
+
+        System.out.println("#### Second Option #### ");
+        StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+                .map(user -> user.getFirstName()).forEach(System.out::println);
     }
 
 }
--- src/main/java/io/javabrains/reactiveworkshop/User.java
+++ src/main/java/io/javabrains/reactiveworkshop/User.java
@@ -1,5 +1,9 @@
 package io.javabrains.reactiveworkshop;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  * This class is a sample custom Java type used in the exercises.
  * DO NOT MODIFY THIS CODE
@@ -7,41 +11,14 @@ package io.javabrains.reactiveworkshop;
  * @author koushikkothagal
  */
 
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
 
     private int id;
     private String firstName;
     private String lastName;
-
-    public User(int id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     @Override
     public String toString() {
